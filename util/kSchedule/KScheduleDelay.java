9029cc088afe84ea6bb52d6778f30df990d7e84d
kiss-yu <oncwnuE3QrpKToJwLhvenpYlTJMw@git.weixin.qq.com>
2019-09-06 15:30:11 +0800

    动态延迟时间支持

---
+++ util/kSchedule/KScheduleDelay.java
@@ -0,0 +1,13 @@
+package com.caishi.v3;
+
+import java.lang.annotation.*;
+
+/**
+ * @author by keray
+ * date:2019/9/6 14:59
+ */
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface KScheduleDelay {
+}
